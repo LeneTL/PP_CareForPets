50b1032dd58d15ee4f612981f8b52d885d5681db
Pastlife-lab <omikron36@hotmail.com>
2025-12-10 12:41:49 +0100

    1

---
+++ PP_Pets/Pet.cs
@@ -0,0 +1,24 @@
+﻿namespace PP_Pets;
+
+internal class Pet
+{
+    private List<Pet> _Pets;
+
+    public void addPet()
+    {
+        _Pets = [];
+    }
+
+    public static void run()
+    {
+        Console.WriteLine("Velg handling for kjøledyret");
+        Console.WriteLine("""
+                          CTRL + C to exit
+                          1. handling 1
+                          2. handling 2
+                          3. handling 3
+                          """);
+    }
+    
+
+}
--- PP_Pets/Program.cs
+++ PP_Pets/Program.cs
@@ -4,10 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Test-Lene");
-            Console.WriteLine("Test2-Lene");
-            Console.WriteLine("Test-Jon");
+            Pet.run();
         }
     }
 }
+
